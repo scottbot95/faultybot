baeaf32a6473a9fd6761cd4b9707d1427a073f8f
Scott Techau <scott.techau@gmail.com>
2023-06-11 16:59:14 -0700

    Update nixpkgs to 23.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A chatbot built by FaultyMuse";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     crane = {
       url = "github:ipetkov/crane";
